14b6b1f61362d662b775a51a38ef06fb8277a864
Lukas <lukasd2000@gmx.de>
2024-02-29 15:41:24 +0100

    Leichte Erweiterung der UI und leichte Backend-Logik.

--- app/src/main/java/com/example/mybarcode/CreateCodeActivity.kt
+++ app/src/main/java/com/example/mybarcode/CreateCodeActivity.kt
@@ -2,10 +2,22 @@ package com.example.mybarcode
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class CreateCodeActivity : AppCompatActivity() {
+    private lateinit var returnButton: Button
+
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create_code)
+        returnButton     = findViewById(R.id.returnFromCreateButton)
+
+
+        returnButton.setOnClickListener {
+            finish()
+        }
     }
 }
