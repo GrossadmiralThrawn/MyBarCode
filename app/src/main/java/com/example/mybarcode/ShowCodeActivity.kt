f01a30ca221e45b3c3be1b09b748c744dbafa74d
Lukas <lukasd2000@gmx.de>
2024-03-26 15:42:51 +0100

    Grobe Speicherndialogstruktur. Nur noch finales speichern erstellen.

--- app/src/main/java/com/example/mybarcode/ShowCodeActivity.kt
+++ app/src/main/java/com/example/mybarcode/ShowCodeActivity.kt
@@ -4,13 +4,18 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.google.zxing.BarcodeFormat
 import com.google.zxing.WriterException
 import com.journeyapps.barcodescanner.BarcodeEncoder
+
+
+
 
 class ShowCodeActivity : AppCompatActivity() {
     private lateinit var returnButton:      Button
@@ -47,10 +52,42 @@ class ShowCodeActivity : AppCompatActivity() {
         }
 
 
+
+        saveButton.setOnClickListener {
+            val options = arrayOf(getString(R.string.save_location_local))
+
+            AlertDialog.Builder(this)
+                .setTitle(getString(R.string.select_save_option))
+                .setItems(options) { _, which ->
+                    when (which) {
+                        0 -> saveLocally()
+                    }
+                }.setNegativeButton(getString(R.string.quit)) { dialog, _ ->
+                    dialog.dismiss()
+                }.show()
+        }
+
+
+
+
         returnButton.setOnClickListener {
             finish()
         }
     }
+
+
+
+    private fun saveToDatabase() {
+        Toast.makeText(this, getString(R.string.save_successfully), Toast.LENGTH_SHORT).show()
+    }
+
+
+
+
+    private fun saveLocally() {
+        Toast.makeText(this, getString(R.string.save_successfully), Toast.LENGTH_SHORT).show()
+    }
+
 
 
 
