678aa5bd3828e62c9f2204a080f353870aaa4fd8
lukas <lukasd2000@gmx.de>
2024-08-02 01:57:08 +0200

    Bugfixes

--- app/src/main/java/com/mybarcode/MainActivity.kt
+++ app/src/main/java/com/mybarcode/MainActivity.kt
@@ -7,7 +7,6 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-import com.example.mybarcode.R
 
 
 class MainActivity : AppCompatActivity() {
